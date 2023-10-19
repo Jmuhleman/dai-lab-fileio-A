d147f9f8a5b1ba8b8ca4c6340aed22b2ac0232ad
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-10-19 11:04:28 +0200

    main class implementation

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,13 +1,15 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.mvitoriacoliveira.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+    private static final String newName = "mvitoriacoliveira";
 
     /**
      * Main method to transform files in a folder.
@@ -33,12 +35,38 @@ public class Main {
         System.out.println("Application started, reading folder " + folder + "...");
         // TODO: implement the main method here
 
+        String PROCESSED_FILE_SUFFIXE = ".processed";
+        FileExplorer currentFolder = new FileExplorer(folder);
+        EncodingSelector encodingSelector = new EncodingSelector();
+        FileReaderWriter mainReaderWriter = new FileReaderWriter();
+        Transformer fileTransformer = new Transformer(newName, wordsPerLine);
+
         while (true) {
             try {
-                // TODO: loop over all files
+                // Get a new file from the FileExplorer,
+                File currentFile = currentFolder.getNewFile();
+                if(currentFile == null) break;
+
+                // Determine its encoding with the EncodingSelector
+                Charset currentFileEncoding = encodingSelector.getEncoding(currentFile);
+                if(currentFileEncoding == null) continue;
+
+                // Read the file with the FileReaderWriter
+                String currentFileContent = mainReaderWriter.readFile(currentFile, currentFileEncoding);
+                if(currentFileContent == null) continue;
+
+                // Transform the content with the Transformer
+                String wrappedAndNumberedText = fileTransformer.wrapAndNumberLines(
+                        fileTransformer.capitalizeWords(fileTransformer.replaceChuck(currentFileContent)));
+
+                // Write the result with the FileReaderWriter
+                // Set new file name: <inputFileName>.<inputFileEncoding> + ".processed"
+                // Result files are written in the same folder as the input files, and encoded with UTF8.
+                File processedFile = new File(currentFile.getAbsolutePath() + PROCESSED_FILE_SUFFIXE);
+                mainReaderWriter.writeFile(processedFile, wrappedAndNumberedText, StandardCharsets.UTF_8);
 
             } catch (Exception e) {
-                System.out.println("Exception: " + e);
+                System.out.println("Exception:" + e);
             }
         }
     }
